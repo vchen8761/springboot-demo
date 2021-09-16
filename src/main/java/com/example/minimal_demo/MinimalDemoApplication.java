f228cd85641bdaf2b380678896f82aa2fa463ccf
cvictor <cvictor@vmware.com>
2021-09-16 11:36:55 -0400

    minimal repoducible spring boot demo with actuator endpoints in swagger-ui

---
+++ src/main/java/com/example/minimal_demo/MinimalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.minimal_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinimalDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minimal_demo/MinimalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minimal_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
